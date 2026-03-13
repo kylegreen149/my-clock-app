1c1725c03c570e275c1d37b9b77372f59aba5209
Kyle Greenidge <kylegreen149@gmail.com>
2026-03-13 14:31:03 -0400

    Get users current time and update every second

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,5 +1,19 @@
+import { useState, useEffect } from "react";
+
 const Clock = () => {
-    return <h1>Clock</h1>
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(id); // cleanup
+  }, []);
+
+  return (
+    <h1>{time.toLocaleTimeString()}</h1>
+  );
 }
 
 export default Clock
